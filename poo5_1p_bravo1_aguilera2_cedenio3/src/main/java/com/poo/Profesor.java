1c888c0c2bb5825caf60e2439e087ddfd4ccb47d
ChristiaAguiilera <lopezrafa211@gmail.com>
2024-11-19 11:02:04 -0500

    correccion de las clases alteradas

--- poo5_1p_bravo1_aguilera2_cedenio3/src/main/java/com/poo/Profesor.java
+++ poo5_1p_bravo1_aguilera2_cedenio3/src/main/java/com/poo/Profesor.java
@@ -1,7 +1,59 @@
 package com.poo;
 import java.util.Scanner;
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Profesor extends Usuario {
+    // Atributos específicos
+    private String facultad;
+    private ArrayList<String> listaMaterias; // Lista de materias que imparte el profesor
 
+    // Constructor
+    public Profesor(int codigo, int cedula, String nombre, String apellido, String usuario, String contrasena, 
+                    String correo, String rol, String facultad, ArrayList<String> listaMaterias) {
+        super(codigo, cedula, nombre, apellido, usuario, contrasena, correo, "PROFESOR"); 
+        this.facultad = facultad;
+        this.listaMaterias = listaMaterias;
+    }
+
+    // Getters y Setters
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public ArrayList<String> getListaMaterias() {
+        return listaMaterias;
+    }
+
+    public void setListaMaterias(ArrayList<String> listaMaterias) {
+        this.listaMaterias = listaMaterias;
+    }
+
+    // Métodos específicos
+    public void reservar(String materia) {
+        if (listaMaterias.contains(materia)) {
+            System.out.println("Reserva realizada por el profesor: " + getNombres() + " para la materia: " + materia);
+            
+        } else {
+            System.out.println("Error: La materia '" + materia + "' no está asociada al profesor.");
+        }
+    }
+
+    public void consultar_reserva() {
+        System.out.println("Consultando reservas realizadas por el profesor: " + getNombres());
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor {" +
+                "Código: " + getCodigo() +
+                ", Nombre: " + getNombres() + " " + getApellidos() +
+                ", Facultad: " + facultad +
+                ", Materias: " + listaMaterias +
+                "}";
+    }
 }
