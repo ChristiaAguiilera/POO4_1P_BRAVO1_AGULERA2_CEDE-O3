656b7d76a6db3c425d2d120a1601e55fe1ced383
Ismael <ismeyumi@espol.edu.ec>
2024-11-26 23:28:42 -0500

    carga de usuarios

--- poo5_1p_bravo1_aguilera2_cedenio3/src/main/java/com/poo/Sistema.java
+++ poo5_1p_bravo1_aguilera2_cedenio3/src/main/java/com/poo/Sistema.java
@@ -5,26 +5,32 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.poo.Enums.Estado;
 import com.poo.Enums.Tipo;
 import com.poo.Usuario.Usuario;
 
 public class Sistema {
-    
     private ArrayList<Usuario> listaUsuario;
     private static ArrayList<Espacio> listaEspacio=new ArrayList<>();
     private ArrayList<Reserva> listaReserva;
     public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
         cargarEspaciosDesdeArchivo("src/main/java/com/poo/Archivos/Espacios.txt");
+        System.out.println("Bienvendio al sistema , Ingrese su usario y contraseña: ");
+        System.out.println("Usario: ");
+        String usuario= sc.nextLine();
+        System.out.println("Contraseña: ");
+        String contraseña= sc.nextLine();
     }
-
     public static void mostrar_menu(){
-
+        System.out.println("Seleccione una de las opciones: ");
+        System.out.println("1...Reservar");
+        System.out.println("2...Consultar reserva");
     }
 
     public static void mostrar_menu_administrador(){
-
 
     }
     public static void cargarEspaciosDesdeArchivo(String nombreArchivo) {
@@ -53,7 +59,6 @@ public class Sistema {
     public  void mostrar_espacios_disponibles() {
         System.out.println("----- Espacios Disponibles -----");
         boolean hayDisponibles = false;
-
         for (Espacio espacio : listaEspacio) {
             if (espacio.getEstado() == Estado.DISPONIBLE) {
                 System.out.println("Código: " + espacio.getCodigo() +
@@ -70,8 +75,27 @@ public class Sistema {
         }
         System.out.println("---------------------------------");
     }
-   
+    public static void CargarUsuarios(String Archivo){
+        try (BufferedReader br = new BufferedReader(new FileReader(Archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split("\\|");
+                int codigo = Integer.parseInt(datos[0].trim());
+                Tipo tipo = Tipo.valueOf(datos[1].trim());
+                String nombre = datos[2].trim();
+                int capacidad = Integer.parseInt(datos[3].trim());
+                Estado estado = Estado.valueOf(datos[4].trim());
+                String permiso = datos[5].trim();
+                // Crear y agregar espacio a la lista
+                Espacio espacio = new Espacio(codigo, tipo, nombre, capacidad, estado, permiso);
+                listaEspacio.add(espacio);
+            }
 
+    }catch(IOException e){
+        e.printStackTrace();
+   
+}
+}
 }
 
 
